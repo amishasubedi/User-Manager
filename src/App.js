0e3678fe311aef90c5b3b5a8e94e95b6216355fe
amishasubedi <amisha.subedi1@gmail.com>
2022-06-12 20:11:19 -0500

    fixed a bug

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   return (
     <div>
       <AddUser onAddUser={addUserHandler} />
-      <UsersList users={[usersList]} />
+      <UsersList users={usersList} />
     </div>
   );
 }
